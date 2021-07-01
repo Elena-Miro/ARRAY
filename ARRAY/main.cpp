18c5b8f75119f91fdf4fad347438e071b078b1de
Elena-Miro <mironenko_ev@mail.ru>
2021-07-01 20:18:43 +0500

    Add project files.

---
+++ ARRAY/main.cpp
@@ -0,0 +1,56 @@
+﻿#include<iostream>
+using namespace std;
+#define tab "\t"
+
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int SIZE = 3;
+	int arr[SIZE];
+	cout << "Введите значение элементов:\n";
+	
+	for (int i = 0; i < SIZE; i++)
+	{
+		cin >> arr[i];
+	}
+	cout << endl;
+
+	cout << "Вывод массива на экран:";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	cout << "Вывод массива на экран в обратном порядке:";
+	for (int i = SIZE-1; i >= 0; i--)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+	double sum1 = 0;
+	double sum2 = 0;
+	
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum1 += arr[i];	
+	}
+	sum2 = sum1 / SIZE;
+	cout << "Среднее арифметическое:"<<sum2;
+	cout << endl;
+
+	int min = arr[0];
+	int max = arr[0];
+
+	for (int i = 1; i < SIZE; i++)
+	{
+		if (min > arr[i])
+			min = arr[i];
+		if (max  < arr[i])
+			max  = arr[i];
+	 }
+	cout << "Минимальное значение:" <<min<<endl ;
+	cout << "Максимальное значение:" << max << endl;
+	
+}
